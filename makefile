52ce5e31e1377e0f2f685a6710df1569f71e49b5
pmolinag <pablomolinagomez@hotmail.com>
2019-07-12 23:44:54 +0200

    Updatin the project structure

--- makefile
+++ makefile
@@ -1,12 +1,12 @@
 #INSTALL ALL DEPENDENCIES
 install:
-	sudo apt-get install python3.6 python3-pip moreutils python3-kivy figlet
-	sudo pip3 install -r requirements.txt
+	@sudo apt-get install python3.6 python3-pip moreutils python3-kivy figlet aircrack-ng
+	@sudo pip3 install -r requirements.txt
 
 #EXECUTE THE APPLICATION
 run:
-	sudo python3 main.py
+	@cd src/view && sudo python3 view.py
 
 #EXECUTE ALL TESTS
 tests:
-	cd test/ && python3 test.py
+	@cd test && python3 test.py
--- test/test.py
+++ test/test.py
@@ -1,21 +1,25 @@
 import unittest
 import sys
 import os
-sys.path.append("..")
-from main import MainWidget
-from src.jammer import Jammer
+from package.src import functions
 
 class TestApp(unittest.TestCase):
 
 	#Test if Type function from MainWidget class works
-	def testType(self):
+	def test_type(self):
 		false = MainWidget.select(self, "other")
 		self.assertEqual(false, False)
 
 	#Test if calcule_time function from Jammer class works
-	def testTime(self):
-		time = Jammer.calcule_time(1)
+	def test_paquets(self):
+		time = Jammer.calcule_paquets(1)
 		self.assertEqual(time, 1831)
+
+	#Test if calcule_time function from Jammer class works
+	def test_paquets_dea(self):
+		time = Jammer.calcule_time(1)
+		self.assertEqual(time, 600000)
+
 
 if __name__ == '__main__':
 	unittest.main()
